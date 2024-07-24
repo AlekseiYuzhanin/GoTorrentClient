ccf70f1fe1692d4d3f9cb5df23a40b3d728b9287
Aleksei Yuzhanin <zombehalk@gmail.com>
2024-07-24 15:22:28 +0300

    commit

--- client/client.go
+++ client/client.go
@@ -3,8 +3,12 @@ package client
 import (
 	"app/bitfield"
 	"app/handshake"
+	"app/message"
 	"app/peers"
+	"bytes"
+	"fmt"
 	"net"
+	"time"
 )
 
 type Client struct {
@@ -17,5 +21,41 @@ type Client struct {
 }
 
 func completeHandshake(conn net.Conn, infoHash, peerID [20]byte) (*handshake.Handshake,error) {
-	
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	req := handshake.New(infoHash, peerID)
+	_, err := conn.Write(req.Serialize())
+	if err != nil{
+		return nil, err
+	}
+	res, err := handshake.Read(conn)
+	if err != nil{
+		return nil, err
+	}
+	if !bytes.Equal(res.InfoHash[:], infoHash[:]){
+		return nil, fmt.Errorf("expected infohash %x but got %x", res.InfoHash, infoHash)
+	}
+
+	return res,nil
 }
+
+func recvBitfield(conn net.Conn) (bitfield.Bitfield, error){
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	msg, err := message.Read(conn)
+	if err != nil{
+		return nil, err
+	}
+	if msg == nil{
+		err := fmt.Errorf("expected bitfield but got %s", msg)
+		return nil,err
+	}
+	if msg.ID != message.MsgBitfield{
+		err := fmt.Errorf("expected bitfield but got ID %d", msg.ID)
+		return nil,err
+	}
+	return msg.Payload,nil
+}
+
--- message/message.go
+++ message/message.go
@@ -47,6 +47,29 @@ func FormatHave(index int) *Message {
 	return &Message{ID: MsgHave, Payload: payload}
 }
 
+func ParsePiece(index int, buf []byte, msg *Message) (int,error){
+	if msg.ID != MsgPiece{
+		return 0, fmt.Errorf("expected PIECE (ID %d), got ID %d", MsgPiece, msg.ID)
+	}
+	if len(msg.Payload) < 8{
+		return 0, fmt.Errorf("payload too short: %d < 8", len(msg.Payload))
+	}
+	parsedIndex := int(binary.BigEndian.Uint32(msg.Payload[0:4]))
+	if parsedIndex != index{
+		return 0, fmt.Errorf("expected index %d: got %d", index,parsedIndex)
+	}
+	begin := int(binary.BigEndian.Uint32(msg.Payload[4:8]))
+	if begin >= len(buf){
+		return 0, fmt.Errorf("begin offset too high. %d >= %d", begin, len(buf))
+	}
+	data := msg.Payload[8:]
+	if begin+len(data) > len(buf){
+		return 0, fmt.Errorf("data to long [%d] for offset %d with length %d", len(data), begin, len(buf))
+	}
+	copy(buf[begin:],data)
+	return len(data), nil
+}
+
 func Read(r io.Reader) (*Message, error){
 	lengthBuf := make([]byte, 4)
 	_, err := io.ReadFull(r, lengthBuf)
@@ -71,6 +94,29 @@ func Read(r io.Reader) (*Message, error){
 	}
 
 	return &m, nil
+}
+
+func ParseHave(msg *Message) (int, error){
+	if msg.ID != MsgHave{
+		return 0, fmt.Errorf("expected HAVE (ID %d), got ID %d",MsgHave,msg.ID)
+	}
+	if len(msg.Payload) != 4{
+		return 0, fmt.Errorf("expected payload length 4, got length %d", len(msg.Payload))
+	}
+	index := int(binary.BigEndian.Uint32(msg.Payload))
+	return index, nil
+}
+
+func (m *Message) Serialize() []byte{
+	if m == nil{
+		return make([]byte, 4)
+	}
+	length := uint32(len(m.Payload) + 1)
+	buf := make([]byte, 4 + length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.ID)
+	copy(buf[5:], m.Payload)
+	return buf
 }
 
 func (m *Message) name() string{
