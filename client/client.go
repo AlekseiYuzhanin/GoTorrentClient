f842e646299bcccd95bae888432ef4ba4456ab72
Aleksei Yuzhanin <zombehalk@gmail.com>
2024-07-24 15:31:46 +0300

    commit

--- client/client.go
+++ client/client.go
@@ -59,3 +59,64 @@ func recvBitfield(conn net.Conn) (bitfield.Bitfield, error){
 	return msg.Payload,nil
 }
 
+func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error){
+	conn,err := net.DialTimeout("tcp", peer.String(), 3 * time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil{
+		conn.Close()
+		return nil, err
+	}
+
+	bf, err := recvBitfield(conn)
+	if err != nil{
+		conn.Close()
+		return nil,err
+	}
+	return &Client{
+		Conn: conn,
+		Choked: true,
+		Bitfield: bf,
+		peer: peer,
+		infoHash: infoHash,
+		peerID: peerID,
+	},nil
+}
+
+func (c *Client) Read() (*message.Message, error){
+	msg, err := message.Read(c.Conn)
+	return msg, err
+}
+
+func (c *Client) SendRequest(index, begin, length int) error {
+	req := message.FormatRequest(index,begin,length)
+	_, err := c.Conn.Write(req.Serialize())
+	return err
+}
+
+func (c *Client) SendIntrested() error{
+	msg := message.Message{ID: message.MsgIntrested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendNotIntrested() error{
+	msg := message.Message{ID: message.MsgNotIntrested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendUnchoke() error{
+	msg := message.Message{ID: message.MsgUnchoke}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendHave(index int) error{
+	msg := message.FormatHave(index)
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
